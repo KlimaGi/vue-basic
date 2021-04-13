946f44e4d4912df1cbe44c5ed4cc0d20de003ae1
Giedre Klimaite <giedre.klima@gmail.com>
2021-04-13 15:53:20 +0300

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,69 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <AddTask @add-task="addTask" />
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+    deleteTask(id) {
+      if (confirm("Are you sure?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+  },
+  created() {
+    this.tasks = [
+      // typically make request
+      {
+        id: 1,
+        text: "yoga",
+        day: "March 1st at 2:30pm",
+        reminder: false,
+      },
+      {
+        id: 2,
+        text: "german language lesson",
+        day: "March 3rd at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "more about git",
+        day: "March 3rd at 11:00am",
+        reminder: true,
+      },
+    ];
   },
 };
 </script>
